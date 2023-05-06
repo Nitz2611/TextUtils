87b281691985270a51abafdec343310ab19b0e83
Nitish sharma <sharmanitish183@gmail.com>
2023-05-06 19:25:35 +0530

    removed active link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,7 +17,7 @@ export default function Navbar(props) {
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link " to="/about">{props.aboutText}</Link>
+                            <Link className="nav-link active " to="/about">{props.aboutText}</Link>
                         </li>
 
 
