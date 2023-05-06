8d964c420a524f85c831ef047cb38dabbdfa361c
Nitish sharma <sharmanitish183@gmail.com>
2023-05-06 19:33:31 +0530

    Adde copy text feature

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -33,6 +33,11 @@ export default function TextForm(props) {
         setText(newText);
         props.showAlert("Words sorted!", "success");
     }
+
+    const handleCopy=()=>{
+        navigator.clipboard.writeText(text);
+        props.showAlert("Copied to clipboard!", "success");
+    }
     const handleOnChange=(event)=>{
         // console.log("On change");
         setText(event.target.value);
@@ -52,6 +57,7 @@ export default function TextForm(props) {
         <button disabled={text.length===0} type="button" className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>convert to Lowercase</button>
         <button disabled={text.length===0} type="button" className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear</button>
         <button disabled={text.length===0} type="button" className="btn btn-primary mx-1 my-1" onClick={handleSortClick}>Sort by words</button>
+        <button disabled={text.length===0} type="button" className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
         </div>
         <div className="container" style={{color: props.mode=== 'dark'? 'white':'black'}}>
             <h2>Your text summary</h2>
